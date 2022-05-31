1e67a3458282c1cc485d39e3d0521151a568df83
Jeremy Banka <hello@jeremybanka.com>
2022-05-30 18:37:23 -0700

    🛸 use react-dom 18 hydration api

--- packages/remix-app/app/entry.client.tsx
+++ packages/remix-app/app/entry.client.tsx
@@ -1,10 +1,8 @@
 import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+import { hydrateRoot } from "react-dom/client";
 import init from "../../rust_functions/build/browser/rust_functions";
 import wasm from "../../rust_functions/build/browser/rust_functions_bg.wasm"
 
-init(wasm).then(() => {
-    hydrate(<RemixBrowser />, document);
-})
+init(wasm).then(() => hydrateRoot(document, <RemixBrowser />))
 
 
